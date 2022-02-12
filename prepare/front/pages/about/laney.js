7016826d7988bb26a91028091ac803b328014552
laney522 <pm72222@gmail.com>
2022-02-12 23:58:54 +0900

    22.02.12 AppLayout.js commit

--- prepare/front/pages/about/laney.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Laney = () => {
-  return (
-    <div>Hello, Laney</div>
-  );
-}
-
-export default Laney;
--- prepare/front/pages/index.js
+++ prepare/front/pages/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import AppLayout from '../components/AppLayout';
 
 const Home = () => {
   return (
-    <div>Hello, next!</div>
+    <AppLayout>
+      <div>Hello, next!</div>
+    </AppLayout>
   );
 }
 
