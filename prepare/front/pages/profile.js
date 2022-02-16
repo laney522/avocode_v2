686bcbe04bd9c3b78b4a268e16360ae6f957b55d
laney522 <pm72222@gmail.com>
2022-02-16 18:33:46 +0900

    add Profile page commit

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -2,13 +2,23 @@ import React from 'react';
 import AppLayout from '../components/AppLayout';
 import Head from 'next/head';
 
+import NicknameEditForm from '../components/NicknameEditForm';
+import FollowList from '../components/FollowList';
+
 const Profile = () => {
+  const followerList = [{ nickname: '레니' }, { nickname: '콜리' }, { nickname: '하품' }];
+  const followingList = [{ nickname: '레니' }, { nickname: '콜리' }, { nickname: '하품' }];
+
   return (
     <>
       <Head>
         <title>내 프로필 | Avocode</title>
       </Head>
-      <AppLayout>내 프로필</AppLayout>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
+      </AppLayout>
     </>
   );
 }
