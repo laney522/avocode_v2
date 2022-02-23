b34f32140314478de5c4fb27fb43a11bbf5ce791
laney522 <pm72222@gmail.com>
2022-02-23 16:01:19 +0900

    FollowButton.js commit

--- prepare/front/components/PostCard.js
+++ prepare/front/components/PostCard.js
@@ -1,15 +1,15 @@
 import React, { useState, useCallback } from 'react';
+import { Card, Button, Avatar, List, Comment, Popover } from 'antd';
 import PropTypes from 'prop-types';
-import { Button, Card, Popover, Avatar, List, Comment } from 'antd';
-import { RetweetOutlined, HeartOutlined, MessageOutlined, EllipsisOutlined } from '@ant-design/icons';
+import { RetweetOutlined, HeartTwoTone, HeartOutlined, MessageOutlined, EllipsisOutlined } from '@ant-design/icons';
+import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import Link from 'next/link';
-import { useDispatch, useSelector } from 'react-redux';
 
 import CommentForm from './CommentForm';
-import PostImages from './PostImages';
 import PostCardContent from './PostCardContent';
-// import FollowButton from './FollowButton';
+import PostImages from './PostImages';
+import FollowButton from './FollowButton';
 import { REMOVE_POST_REQUEST } from '../reducers/post';
 
 const CardWrapper = styled.div`
@@ -43,52 +43,61 @@ const PostCard = ({ post }) => {
     <CardWrapper key={post.id}>
       <Card
         cover={post.Images[0] && <PostImages images={post.Images} />}
-        actions={[                                // 배열안에 JSX를 넣어줄때는 항상 key를 넣어준다.
+        actions={[
           <RetweetOutlined key="retweet" />,
           liked
             ? <HeartTwoTone twoToneColor="#eb2f96" key="heart" onClick={onToggleLike} />
             : <HeartOutlined key="heart" onClick={onToggleLike} />,
           <MessageOutlined key="message" onClick={onToggleComment} />,
-          <Popover key="more" content={(
-            <Button.Group>
-              {id && post.User.id === id
-                ? (
-                  <>
-                    <Button>수정</Button>
-                    <Button type='danger' loading={removePostLoading} onClick={onRemovePost}>삭제</Button>
-                  </>
-                )
-                : <Button>신고</Button>}
-            </Button.Group>
-          )}>
+          <Popover
+            key="ellipsis"
+            content={(
+              <Button.Group>
+                {id && post.UserId === id
+                  ? (
+                    <>
+                      <Button>수정</Button>
+                      <Button type="danger" loading={removePostLoading} onClick={onRemovePost}>삭제</Button>
+                    </>
+                  )
+                  : <Button>신고</Button>}
+              </Button.Group>
+            )}
+          >
             <EllipsisOutlined />
-          </Popover>
+          </Popover>,
         ]}
+        extra={<FollowButton post={post} />}
       >
         <Card.Meta
-          avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
+          avatar={<Avatar>{ /* post.User.nickname[0] */ }</Avatar>}
           title={post.User.nickname}
           description={<PostCardContent postData={post.content} />}
         />
       </Card>
       {commentFormOpened && (
-        <div>
+        <>
           <CommentForm post={post} />
           <List
-            header={`${post.Comments.length}개의 댓글`}
+            header={`${post.Comments ? post.Comments.length : 0} 댓글`}
             itemLayout="horizontal"
-            dataSource={post.Comments}
+            dataSource={post.Comments || []}
             renderItem={(item) => (
               <li>
                 <Comment
                   author={item.User.nickname}
-                  avatar={<Avatar>{item.User.nickname[0]}</Avatar>}
+                  avatar={(
+                    <Link href={{ pathname: '/user', query: { id: item.User.id } }} as={`/user/${item.User.id}`}>
+                      <a><Avatar>{item.User.nickname[0]}</Avatar></a>
+                    </Link>
+                  )}
                   content={item.content}
                 />
               </li>
             )}
           />
-        </div>)}
+        </>
+      )}
     </CardWrapper>
   );
 };
