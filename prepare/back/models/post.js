223854eacff1c01b272a32ff92ae5fcbabed04d0
laney522 <pm72222@gmail.com>
2022-03-01 22:36:03 +0900

    add sequelize commit

---
+++ prepare/back/models/post.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+  const Post = sequelize.define('Post', {
+    // id가 기본적으로 들어있다.
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  }, {
+    charset: 'utf8mb4',
+    collate: 'utf8mb4_general_ci', // mb4 - 이모티콘 저장
+  });
+  Post.associate = (db) => {};
+  return Post;
+}
