0b86a6db5b671166789e86513ee45805bd931705
laney522 <pm72222@gmail.com>
2022-03-04 00:11:44 +0900

    update user.js router commit

--- prepare/back/routes/user.js
+++ prepare/back/routes/user.js
@@ -1,15 +1,36 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const { User } = require('../models');
+const { nextTick } = require('process');
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {  // post /user
+router.post('/', async (req, res, next) => {  // post /user
+ try {
+   const exUser = await User.findOne({
+     where: {
+       email: req.body.email,
+     }
+   });
+   if (exUser) {
+     return res.status(403).send('이미 사용중인 아이디입니다.');
+   }
+  const hashedPassword = await bcrypt.hash(req.body.password, 12);
   await User.create({
     email: req.body.nickname,
     nickname: req.body.nickname,
-    password: req.body.password,
+    password: hashedPassword,
   });
-  res.send('ok');
+  res.status(200).send('ok');
+ } catch (error) {
+   console.error(error);
+   next(error);
+ }
 });
 
 module.exports = router;
+
+// 200 성공
+// 300 리다이렉트
+// 400 클라이언트 에러
+// 500 서버 에러
