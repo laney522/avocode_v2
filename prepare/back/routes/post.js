26a16ee5a662bd3d8fa0637220ee59ccee1b83fc
laney522 <pm72222@gmail.com>
2022-03-12 17:16:50 +0900

    update post.js commit

--- prepare/back/routes/post.js
+++ prepare/back/routes/post.js
@@ -6,14 +6,36 @@ const express = require('express');
   // })
   
 const { Post } = require('../models');
+const { isLoggedIn } = require('./middlewares');
   
 const router = express.Router();
-router.post('/', async (req, res, next) => {
+router.post('/', isLoggedIn, async (req, res, next) => {
   try {
     const post = await Post.create({
       content: req.body.content,
+      UserId: req.user.id,
     });
     res.status(201).json(post);
+  } catch(error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.post('/:postId/comment', isLoggedIn, async (req, res, next) => { // POST / post/1/comment
+  try {
+    const post = await Post.findOne({
+      where: { id: req.params.postId }
+    });
+    if (!post) {
+      return res.status(403).send('존재하지 않는 게시글입니다.');
+    }
+    const comment = await Comment.create({
+      content: req.body.content,
+      PostId: req.params.postId, // reducer > post.js > ADD_COMMENT_SUCCESS -> action.data.PostId 대소문자 일치시켜주어야 한다!
+      UserId: req.user.id,
+    })
+    res.status(201).json(comment);
   } catch(error) {
     console.error(error);
     next(error);
