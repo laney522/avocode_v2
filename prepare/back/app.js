fdae7bb125c1304a44773cff60bf9d2a326e4680
laney522 <pm72222@gmail.com>
2022-03-21 10:31:04 +0900

    cors err

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(morgan('dev'));
 }
 app.use(cors({
-  origin: ['http://localhost:3060', 'http://avocode.site'],
+  origin: ['http://localhost:3060', 'http://avocode.site', 'http://54.180.17.237'],
   credentials: true,
 }));
 app.use('/', express.static(path.join(__dirname, 'uploads')));
