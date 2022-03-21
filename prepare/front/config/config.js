1dd7cc4c0d4bb43951b1b7cbbc79d5c7273b912a
laney522 <pm72222@gmail.com>
2022-03-21 10:42:02 +0900

    change to .site

--- prepare/front/config/config.js
+++ prepare/front/config/config.js
@@ -1,3 +1,3 @@
-export const backUrl = 'http://localhost:3065';
+// export const backUrl = 'http://localhost:3065';
 
-// export const backUrl = 'http://api.avocode.site';
+export const backUrl = 'http://api.avocode.site';
